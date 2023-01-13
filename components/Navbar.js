bb71779e00b029f5ee08bbc8ceabe41e60081ecb
ali <aliahanch70@gfmail.com>
2023-01-13 21:32:29 +0330

    nav md

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,22 +3,22 @@ import React, {Component} from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="p-5 bg-blue-800  shadow md:flex md:items-center md:justify-between">
+            <nav className="p-5 bg-[#4C1D95]  shadow md:flex md:items-center md:justify-between">
                 <div>
-                    <span className="text-2xl text-cyan-200">
+                    <span className="text-2xl text-[#0EA5E9]">
                         Movie 10
                     </span>
                 </div>
 
-                <ul className="md:flex md:items-center md:opacity-100 opacity-0 md:w-auto md:py-0 py-4 absolute md:pl-0 pl-7">
+                <ul className="right-0 md:flex md:items-center md:opacity-100 opacity-0 md:w-auto md:py-0 py-4 absolute md:pl-0 pl-7">
                     <li className="mx-4 my-6 md:my-0 ">
-                        <a href="#" className="text-xl text-cyan-500 hover:text-cyan-300 duration-500">Home</a>
+                        <a href="#" className="text-xl text-[#0EA5E9] hover:text-cyan-300 duration-500">Home</a>
                     </li >
                     <li className="mx-4 my-6 md:my-0">
-                        <a href="#" className="text-xl text-cyan-500 hover:text-cyan-300 duration-500">Serials</a>
+                        <a href="#" className="text-xl text-[#0EA5E9] hover:text-cyan-300 duration-500">Serials</a>
                     </li>
                     <li className="mx-4 my-6 md:my-0">
-                        <a href="#" className="text-xl text-cyan-500 hover:text-cyan-300 duration-500">Movies</a>
+                        <a href="#" className="text-xl text-[#0EA5E9] hover:text-cyan-300 duration-500">Movies</a>
                     </li>
 
                 </ul>
