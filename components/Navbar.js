40ffb90259404373890cae08d696db5539a5b532
ali <aliahanch70@gfmail.com>
2023-01-13 20:56:10 +0330

    nav

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="p-5 bg-blue-800  shadow">
+                <span className="text-2xl font-[Poppins]">
+                    Movie 10
+                </span>
+            </nav>
+
+        );
+    }
+}
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
-
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <>
+          <Navbar/>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+      </>
   )
 }
