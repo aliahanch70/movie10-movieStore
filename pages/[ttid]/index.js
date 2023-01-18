53405a077103df3639d0358474e439632b67ec4b
user <aliahanch70@gmail.com>
2023-01-18 11:05:09 +0330

    getServerSideProps update

--- pages/[ttid]/index.js
+++ pages/[ttid]/index.js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import {useState} from "react"
 import Detail from "../../components/Detail";
 
-const Post = () => {
+const Post = ({data}) => {
     const router = useRouter()
     const { ttid } = router.query
     const [movie , setMovie] = useState([]);
@@ -13,20 +13,22 @@ const Post = () => {
     };
 
 
-    async function fMovie() {
-        await fetch('https://www.omdbapi.com/?i=' + ttid+"&apikey=221cf49a",options)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                setMovie(response)
-
-            })
-            .catch(err => console.error(err));
-    }
-    if(fe === 0){ fMovie()
+    // async function fMovie() {
+    //     await fetch('https://www.omdbapi.com/?i=' + ttid+"&apikey=221cf49a",options)
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             console.log(response);
+    //             setMovie(response)
+    //
+    //         })
+    //         .catch(err => console.error(err));
+    // }
+    if(fe === 0){
+        // fMovie()
+        setMovie(data)
         setFe(1)
     }
-
+    console.log(data);
     return( <>
             <div className="items-center">
                 ssd: {movie.Title}
@@ -38,5 +40,41 @@ const Post = () => {
     </>
     )
 }
+
+export async function getServerSideProps(x) {
+    const options = {
+        method: 'GET'
+    };
+
+    const { ttid } = x.query
+    // Fetch data from external API
+    const res = await fetch('https://www.omdbapi.com/?i=' + ttid+"&apikey=221cf49a",options)
+    const data = await res.json()
+    console.log("1")
+
+    // Pass data to the page via props
+    return { props: { data } }
+}
+
+// export async function getStaticProps(x) {
+//     // Call an external API endpoint to get posts
+//     const options = {
+//         method: 'GET'
+//     };
+//
+//     const { ttid } = x.query
+//     // Fetch data from external API
+//     const res = await fetch('https://www.omdbapi.com/?i=' + ttid+"&apikey=221cf49a",options)
+//     const data = await res.json()
+//     console.log("1")
+//
+//     // By returning { props: { posts } }, the Blog component
+//     // will receive `posts` as a prop at build time
+//     return {
+//         props: {
+//             data,
+//         },
+//     }
+// }
 
 export default Post;
